9b4e22e5fcd1ec857b6f8f9fc3cfd22ad3b0b507
9K720 Iskander <67781542+ijaha@users.noreply.github.com>
2021-04-12 23:53:43 +0300

    Add files via upload

---
+++ 16.1.py
@@ -0,0 +1,6 @@
+# Используя функцию randrange(plustilino)) получите псевдослучайное четное число в пределах от 6 до 12.
+# Также получите число кратное пяти в пределах от 5 до 100.
+
+from random import randrange
+print(randrange(6, 12, 2))
+print(randrange(5, 100, 5))
